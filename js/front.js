a4a0a141d02a2a7e696240fb5368f9d9a35e852e
Luisa-VT <luisa.m.truembach@stud.h-da.de>
2021-03-02 18:31:09 +0100

    cookies

--- js/front.js
+++ js/front.js
@@ -3,12 +3,13 @@ $(function () {
         sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
         easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-        animationTime: 1000, // AnimationTime let you define how long each section takes to animate
+        animationTime: 700, // AnimationTime let you define how long each section takes to animate
         pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
         updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
         beforeMove: function (index) {
         }, // This option accepts a callback function. The function will be called before the page moves.
         afterMove: function (index) {
+            setCookie('lastpage',index,7)
         }, // This option accepts a callback function. The function will be called after the page moves.
         loop: true, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
         keyboard: true, // You can activate the keyboard controls
@@ -17,9 +18,36 @@ $(function () {
         // the browser's width is less than 600, the fallback will kick in.
         direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
     });
-
-
+    var x = getCookie('lastpage');
+    if (x) {
+        $('.main').moveTo(x);
+    }
     lightbox.option({
        'resizeDuration': 200
     });
 });
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+setCookie('ppkcookie','testcookie',7);
+
