d9af7af9b36a3f1025cb9c21a144faf0a8a7b262
GrueFlo <58940174+GrueFlo@users.noreply.github.com>
2021-03-03 17:17:11 +0100

    repair

--- js/front.js
+++ js/front.js
@@ -104,8 +104,6 @@ function show_SoundMessage() {
   var soundfile = "../sound/one_vibration.mp3";
   var sound_id = "vibration";
   var sound_message = "<";
-  sound_message += 'embed src="' + soundfile + '" autostart="true" 
-  loop="false" hidden="true" height="0" width="0"';
   sound_message += ">";
   document.getElementById(sound_id).innerHTML = sound_message;
 }
